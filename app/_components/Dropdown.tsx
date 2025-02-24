57ca87c14934adc37027fd9924981fd8c63bcebd
leehj322 <gudwns8956@naver.com>
2025-02-24 20:51:27 +0900

    Feat: 드롭다운 애니메이션 추가 #1
    - 드롭다운 open, close 애니메이션 keyframe 추가 (tailwind config)
    - 드롭다운 close 애니메이션을 위한 타이머 사용

--- app/_components/Dropdown.tsx
+++ app/_components/Dropdown.tsx
@@ -1,8 +1,8 @@
 import useToggle from "@/hooks/useToggle";
 import getElementsFromChildren from "@/utils/getElementsFromChildren";
-import { ReactNode, useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import Slot from "./Slot";
-import { twMerge } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 
 interface DropdownTriggerProps extends React.HTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
@@ -25,12 +25,16 @@ interface DropdownContentProps {
   children?: ReactNode;
   className?: string;
   onClick?: () => void;
+  "data-state"?: "open" | "closed";
+  position?: "upper" | "under";
 }
 
 export function DropdownContent({
   children,
   className,
   onClick,
+  "data-state": dataState,
+  position = "under",
 }: DropdownContentProps) {
   const DropdownItemElements = getElementsFromChildren({
     children,
@@ -38,9 +42,27 @@ export function DropdownContent({
   });
 
   return (
-    <ul className={twMerge("absolute top-[100%]", className)} onClick={onClick}>
-      {DropdownItemElements}
-    </ul>
+    <div
+      className={twJoin(
+        "absolute overflow-hidden",
+        position === "under" && "top-[100%]",
+        position === "upper" && "bottom-[100%]",
+      )}
+    >
+      <ul
+        className={twMerge(
+          position === "under" &&
+            "data-[state=closed]:animate-slide-up-out data-[state=open]:animate-slide-down-in",
+          position === "upper" &&
+            "data-[state=closed]:animate-slide-down-out data-[state=open]:animate-slide-up-in",
+          className,
+        )}
+        onClick={onClick}
+        data-state={dataState}
+      >
+        {DropdownItemElements}
+      </ul>
+    </div>
   );
 }
 
@@ -59,6 +81,7 @@ interface DropdownProps {
 
 export function Dropdown({ children }: DropdownProps) {
   const [isOpen, toggleIsOpen, setIsOpen] = useToggle(false);
+  const [isAnimating, setIsAnimating] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const TriggerElement = getElementsFromChildren({
@@ -87,11 +110,28 @@ export function Dropdown({ children }: DropdownProps) {
     return () => document.removeEventListener("click", handleClickOutside);
   }, []);
 
+  // close animation을 위한 타이머 설정
+  useEffect(() => {
+    if (isOpen) {
+      setIsAnimating(true);
+      return;
+    }
+
+    const timer = setTimeout(() => setIsAnimating(false), 150);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isOpen]);
+
   return (
     <div className="relative" ref={dropdownRef}>
       <Slot onClick={toggleIsOpen}>{TriggerElement}</Slot>
-      {isOpen && (
-        <Slot className="cursor-pointer" onClick={closeDropdown}>
+      {(isOpen || (!isOpen && isAnimating)) && (
+        <Slot
+          className="cursor-pointer"
+          onClick={closeDropdown}
+          data-state={isOpen ? "open" : "closed"}
+        >
           {ContentElement}
         </Slot>
       )}
