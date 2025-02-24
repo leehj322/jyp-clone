38269c3ed58f07fd4e39ada4669daece8fec3890
leehj322 <gudwns8956@naver.com>
2025-02-24 20:51:27 +0900

    Feat: 드롭다운 애니메이션 추가 #1
    - 드롭다운 open, close 애니메이션 keyframe 추가 (tailwind config)
    - 드롭다운 close 애니메이션을 위한 타이머 사용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,30 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        "slide-down-in": {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0%)" },
+        },
+        "slide-up-out": {
+          "0%": { transform: "translateY(0%)" },
+          "100%": { transform: "translateY(-100%)" },
+        },
+        "slide-down-out": {
+          "0%": { transform: "translateY(0%)" },
+          "100%": { transform: "translateY(100%)" },
+        },
+        "slide-up-in": {
+          "0%": { transform: "translateY(100%)" },
+          "100%": { transform: "translateY(0%)" },
+        },
+      },
+      animation: {
+        "slide-down-in": "slide-down-in 0.4s ease-in-out",
+        "slide-up-in": "slide-up-in 0.4s ease-in-out",
+        "slide-up-out": "slide-up-out 0.2s ease-in-out",
+        "slide-down-out": "slide-down-out 0.2s ease-in-out",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
