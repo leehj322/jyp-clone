1bcce812b153cd297dea9872aabc02e5494d2a45
leehj322 <gudwns8956@naver.com>
2025-02-25 16:03:27 +0900

    Feat: 캐러샐 네비게이터 구현 #2
    - Carousel navigation 구현 (애니메이션 및 끝 페이지 이동 미구현)
    - 화살표 아이콘 이미지 추가

--- app/_components/Carousel.tsx
+++ app/_components/Carousel.tsx
@@ -1,5 +1,7 @@
 import getElementsFromChildren from "@/utils/getElementsFromChildren";
-import { ReactNode } from "react";
+import padZero from "@/utils/padZero";
+import Image from "next/image";
+import { ReactNode, useState, MouseEvent } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface CarouselBackgroundProps {
@@ -83,10 +85,26 @@ interface CarouselProps {
 }
 
 export function Carousel({ children, className }: CarouselProps) {
+  const [currentSlide, setCurrentSlide] = useState(1);
+
   const ItemElements = getElementsFromChildren({
     children,
     targetElement: <CarouselItem />,
   });
+
+  const slidesCount = ItemElements.length;
+
+  const handlePrevButtonClick = (e: MouseEvent) => {
+    e.preventDefault();
+    if (currentSlide === 1) return;
+    setCurrentSlide((prev) => prev - 1);
+  };
+
+  const handleNextButtonClick = (e: MouseEvent) => {
+    e.preventDefault();
+    if (currentSlide === slidesCount) return;
+    setCurrentSlide((prev) => prev + 1);
+  };
 
   return (
     <section
@@ -95,7 +113,78 @@ export function Carousel({ children, className }: CarouselProps) {
         className,
       )}
     >
-      <div className="absolute flex h-full">{ItemElements}</div>
+      <div
+        className="absolute flex h-full"
+        style={{ transform: `translateX(-${(currentSlide - 1) * 100}vw)` }}
+      >
+        {ItemElements}
+      </div>
+      <CarouselNavigation
+        currentSlide={currentSlide}
+        slidesCount={slidesCount}
+        onPrevButtonClick={handlePrevButtonClick}
+        onNextButtonClick={handleNextButtonClick}
+      />
     </section>
+  );
+}
+
+interface CarouselNavigationProps {
+  currentSlide: number;
+  slidesCount: number;
+  onPrevButtonClick: (e: MouseEvent) => void;
+  onNextButtonClick: (e: MouseEvent) => void;
+}
+
+function CarouselNavigation({
+  currentSlide,
+  slidesCount,
+  onPrevButtonClick,
+  onNextButtonClick,
+}: CarouselNavigationProps) {
+  const paddedCurrentSlideNumber = padZero(currentSlide, 2);
+  const paddedSlidesCount = padZero(slidesCount, 2);
+
+  return (
+    <>
+      <div className="wrapper absolute h-full w-full">
+        <button
+          className="carousel-button-prev absolute bottom-0 left-0 top-0 hidden w-[20%] lg:block"
+          onClick={onPrevButtonClick}
+        >
+          <div className="carousel-pagination absolute left-10 top-[45%] hidden gap-2 text-white lg:flex">
+            <Image
+              src="/icons/fi-sr-caret-left.svg"
+              alt="carousel prev arrow"
+              width={16}
+              height={16}
+            />
+            <div className="text-[33px]">
+              {paddedCurrentSlideNumber}
+              <span className="mx-1">/</span>
+              <span className="text-[21px]">{paddedSlidesCount}</span>
+            </div>
+          </div>
+        </button>
+        <button
+          className="carousel-button-next absolute bottom-0 right-0 top-0 hidden w-[20%] lg:block"
+          onClick={onNextButtonClick}
+        >
+          <div className="carousel-navigation absolute right-10 top-[45%] hidden gap-2 text-white lg:flex">
+            <div className="text-[14px] font-normal text-white">NEXT</div>
+            <Image
+              src="/icons/fi-sr-caret-right.svg"
+              alt="carousel next arrow"
+              width={16}
+              height={16}
+            />
+          </div>
+        </button>
+        <div className="carousel-mobile-pagination px-auto absolute bottom-0 flex h-16 w-full items-center justify-center gap-8 text-[18px] font-light text-white lg:hidden">
+          <span>{paddedCurrentSlideNumber}</span>
+          <span>{paddedSlidesCount}</span>
+        </div>
+      </div>
+    </>
   );
 }
