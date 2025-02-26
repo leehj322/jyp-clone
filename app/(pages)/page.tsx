221b21ec28e0879691f53a7e4c662239717c5199
leehj322 <gudwns8956@naver.com>
2025-02-27 01:30:42 +0900

    Move: 메인 페이지 page.tsx 파일 위치 변경 #15

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <div className="absolute h-[100vh] w-[100vw] bg-black" />
+      <Header />
+      <Footer />
+    </>
+  );
+}
