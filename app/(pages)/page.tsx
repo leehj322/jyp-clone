b310fefe0f1b1b0d8ced1fd64060373f157aabf1
leehj322 <gudwns8956@naver.com>
2025-03-06 01:45:46 +0900

    Feat: 기본 메타 데이터 추가 #37

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,6 +2,11 @@ import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import MainCarousel from "./_components/MainCarousel";
 
+export const metadata = {
+  title: "JYP Entertainment",
+  description: "JYP 엔터테인먼트 클론",
+};
+
 export default function Home() {
   return (
     <>
